6970ff99fede4dc56151d2b9906c3af802092d20
JamesLonely <jameslonely0515@gmail.com>
2014-11-26 16:58:52 +0800

    git test

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,39 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var myVariable = 42
+myVariable=50
+let myConstant:Float  = 4
+
+let label = "Hi , \(myVariable+3)"
+let showStr= label + String(myVariable)
+
+var ary = ["123","321","999"]
+ary[1] = "11111"
+println(ary)
+
+
+var dist = [23:"age","mike":"name"]
+dist["name"] = "qweasdasd"
+
+
+
+// 條件判斷
+for ex in ary{
+
+    if ex == "123" {
+        println("have 123")
+    }else{
+        println("no 123")
+    }
+
+}
+
+
+
+
+
+
+
---
+++ swift_Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift_Practice
+//
+//  Created by James on 2014/11/24.
+//  Copyright (c) 2014年 James. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
